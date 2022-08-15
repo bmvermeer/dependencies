2fa7ec8141bdbe611f5454103905bff54d0db424
Brian Vermeer <brianvermeer@snyk.io>
2022-08-15 17:59:17 +0200

    Initial commit

---
+++ maven/depMaven/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.eclipse.collections.api.list.primitive.IntList;
+import org.eclipse.collections.impl.factory.primitive.IntLists;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IntList list = IntLists.immutable.empty();
+    }
+}
