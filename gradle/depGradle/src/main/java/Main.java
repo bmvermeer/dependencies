6953cb3e5103d1c9e6ef4dfe635bfe408c331172
Brian Vermeer <brianvermeer@snyk.io>
2022-08-15 18:13:51 +0200

    commit2

---
+++ gradle/depGradle/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.eclipse.collections.api.list.primitive.DoubleList;
+import org.eclipse.collections.impl.factory.primitive.DoubleLists;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoubleList list = DoubleLists.immutable.empty();
+    }
+}
